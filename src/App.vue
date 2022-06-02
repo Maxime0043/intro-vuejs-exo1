0bbbfd737f7176538c83cb56c8343a8d5e4c6ecf
Pinna Maxime <max.pinna@hotmail.fr>
2022-06-02 15:07:01 +0200

    OK : Exo 6 - Implémentation condition affichage

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
 import Compteur from "@/components/Compteur.vue";
 import DemoJWT from "@/components/DemoJWT.vue";
 import Tableau from "@/components/Tableau.vue";
+import Connexion from "@/components/Connexion.vue";
 </script>
 
 <template>
   <div>
-    <!-- <Compteur />
-    <hr />
-    <DemoJWT />
-    <hr /> -->
-    <Tableau />
+    <!-- <Compteur /> -->
+    <!-- <hr /> -->
+    <!-- <DemoJWT /> -->
+    <!-- <hr /> -->
+    <!-- <Tableau /> -->
+    <Connexion />
   </div>
 </template>
 
---
+++ src/components/Connexion.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+
+const user = ref(null);
+
+function login() {
+  user.value = "Maxime";
+}
+
+function logout() {
+  user.value = null;
+}
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <hr />
+    <h2 v-if="user === null">Hello Anonyme</h2>
+    <h2 v-else>Hello {{ user }}</h2>
+    <button v-if="user === null" @click="login()">Se connecter</button>
+    <button v-else @click="logout()">Se déconnecter</button>
+  </div>
+</template>
+
+<style>
+</style>
