4a02ebb2bd13e4abf4b38827ff794060137c4540
Pinna Maxime <max.pinna@hotmail.fr>
2022-06-02 10:04:00 +0200

    OK: Exo 1 - Création d'un compteur qui s'incrémente toutes les secondes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
+import { ref } from "vue";
+
+const compteur = ref(0);
+
+setInterval(() => {
+  compteur.value++;
+}, 1000);
 </script>
 
-<template></template>
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <p>{{ compteur }}</p>
+  </div>
+</template>
 
-<style>
-</style>
+<style></style>
