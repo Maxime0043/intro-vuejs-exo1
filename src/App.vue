eafcb6297621a2129db88c2dbd69b0c71d9a38a3
Pinna Maxime <max.pinna@hotmail.fr>
2022-06-02 10:34:33 +0200

    OK : Exo 4 - Implémentation d'une sauvegarde d'un JWT dans le localStorage + affichage du contenu du token s'il est définit

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,16 @@ import { ref } from "vue";
 import * as jose from "jose";
 
 const compteur = ref(0);
-const token = ref("");
+
+const tokenToDecode = ref("");
 const decodedToken = ref("");
+
+const token = ref(localStorage.getItem("token") ?? "");
+const connectedUser = ref(null);
+
+if (token.value) {
+  connectedUser.value = jose.decodeJwt(token.value);
+}
 
 setInterval(() => {
   compteur.value++;
@@ -15,8 +23,12 @@ function resetCompteur() {
 }
 
 function decodeToken() {
-  const claims = jose.decodeJwt(token.value);
+  const claims = jose.decodeJwt(tokenToDecode.value);
   decodedToken.value = JSON.stringify(claims);
+}
+
+function saveToken() {
+  localStorage.setItem("token", token.value);
 }
 </script>
 
@@ -32,10 +44,19 @@ function decodeToken() {
 
     <div class="column">
       <label>Token</label>
-      <textarea v-model="token"></textarea>
+      <textarea v-model="tokenToDecode"></textarea>
       <button v-on:click="decodeToken()">Décoder</button>
       <label>Décodage des claims</label>
       <textarea v-model="decodedToken"></textarea>
+    </div>
+
+    <div class="column">
+      <label>Token à enregistrer</label>
+      <textarea v-model="token"></textarea>
+      <button v-on:click="saveToken()">Enregistrer</button>
+      <textarea v-if="connectedUser !== null" readonly>{{
+        JSON.stringify(connectedUser)
+      }}</textarea>
     </div>
   </div>
 </template>
