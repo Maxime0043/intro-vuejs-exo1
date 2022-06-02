395b520c1327e6ece59d4707bfed7336d26f48d6
Pinna Maxime <max.pinna@hotmail.fr>
2022-06-02 10:05:06 +0200

    OK: Exo 2 - Création d'un bouton pour reset le compteur + champs texte qui est lié à la variable compteur

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,18 @@ const compteur = ref(0);
 setInterval(() => {
   compteur.value++;
 }, 1000);
+
+function resetCompteur() {
+  compteur.value = 0;
+}
 </script>
 
 <template>
   <div>
     <h1>Hello World</h1>
     <p>{{ compteur }}</p>
+    <button v-on:click="resetCompteur()">Reset Compteur</button>
+    <input type="number" v-model="compteur" />
   </div>
 </template>
 
