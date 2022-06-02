6afc3e0871075734c4821c7985062f47d24c862f
Pinna Maxime <max.pinna@hotmail.fr>
2022-06-02 12:28:27 +0200

    OK : Exo 5 - Création d'un tableau (v-for) + Insertion d'une valeur avec un champs 'nom' et 'prenom'

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import Compteur from "@/components/Compteur.vue";
 import DemoJWT from "@/components/DemoJWT.vue";
+import Tableau from "@/components/Tableau.vue";
 </script>
 
 <template>
   <div>
-    <Compteur />
+    <!-- <Compteur />
     <hr />
     <DemoJWT />
+    <hr /> -->
+    <Tableau />
   </div>
 </template>
 
---
+++ src/components/Tableau.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from "vue";
+
+const newObjetLastName = ref("");
+const newObjetFirstName = ref("");
+const listeObjets = ref([
+  { id: 0, nom: "nom1", prenom: "Alice" },
+  { id: 1, nom: "nom2", prenom: "Bob" },
+  { id: 2, nom: "nom3", prenom: "Charlie" },
+]);
+
+function ajouter() {
+  let id = listeObjets.value[listeObjets.value.length - 1]?.id;
+
+  if (!id) id = 0;
+  else id++;
+
+  listeObjets.value.push({
+    id: id,
+    nom: newObjetLastName.value,
+    prenom: newObjetFirstName.value,
+  });
+}
+
+function supprimer(id) {
+  listeObjets.value = listeObjets.value.filter((obj) => obj.id !== id);
+}
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="newObjetLastName" placeholder="Nom" />
+    <input type="text" v-model="newObjetFirstName" placeholder="Prénom" />
+    <button @click="ajouter()">Ajouter</button>
+
+    <div
+      v-for="monObjet in listeObjets"
+      :key="monObjet.id"
+      @click="supprimer(monObjet.id)"
+    >
+      <h2>{{ monObjet.nom }} {{ monObjet.prenom }}</h2>
+      <p>{{ monObjet.id }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
