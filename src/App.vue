e9070575f456343c90ad5740fc3b4885465be820
Pinna Maxime <max.pinna@hotmail.fr>
2022-06-02 10:17:42 +0200

    OK : Exo 3 - Implémentation d'un champs texte pour pouvoir décoder les claims d'un JWT

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { ref } from "vue";
+import * as jose from "jose";
 
 const compteur = ref(0);
+const token = ref("");
+const decodedToken = ref("");
 
 setInterval(() => {
   compteur.value++;
@@ -10,15 +13,39 @@ setInterval(() => {
 function resetCompteur() {
   compteur.value = 0;
 }
+
+function decodeToken() {
+  const claims = jose.decodeJwt(token.value);
+  decodedToken.value = JSON.stringify(claims);
+}
 </script>
 
 <template>
   <div>
     <h1>Hello World</h1>
     <p>{{ compteur }}</p>
-    <button v-on:click="resetCompteur()">Reset Compteur</button>
-    <input type="number" v-model="compteur" />
+
+    <div>
+      <button v-on:click="resetCompteur()">Reset Compteur</button>
+      <input type="number" v-model="compteur" />
+    </div>
+
+    <div class="column">
+      <label>Token</label>
+      <textarea v-model="token"></textarea>
+      <button v-on:click="decodeToken()">Décoder</button>
+      <label>Décodage des claims</label>
+      <textarea v-model="decodedToken"></textarea>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.column {
+  display: flex;
+  flex-direction: column;
+}
+.column > * {
+  width: max-content;
+}
+</style>
